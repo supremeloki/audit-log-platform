e2205e321ca4ab2fcd3b6600c2b7b96e2213d439
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-07-07 12:49:56 +0330

    initial package layout

--- src/audit_platform/__init__.py
+++ src/audit_platform/__init__.py
@@ -1,19 +1,1 @@
 from .core import (
-    AuditPlatform,
-    AuditPlatformError,
-    AuditRecord,
-    InMemorySink,
-    JsonlFileSink,
-    TamperDetectedError,
-)
-
-__all__ = [
-    "AuditPlatform",
-    "AuditPlatformError",
-    "AuditRecord",
-    "InMemorySink",
-    "JsonlFileSink",
-    "TamperDetectedError",
-]
-
-__version__ = "0.1.0"

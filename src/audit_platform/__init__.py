9c990c8e6113a51c91580540347c6760456bcf8d
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-09-04 23:30:44 +0330

    final quality pass

--- src/audit_platform/__init__.py
+++ src/audit_platform/__init__.py
@@ -2,3 +2,18 @@ from .core import (
     AuditPlatform,
     AuditPlatformError,
     AuditRecord,
+    InMemorySink,
+    JsonlFileSink,
+    TamperDetectedError,
+)
+
+__all__ = [
+    "AuditPlatform",
+    "AuditPlatformError",
+    "AuditRecord",
+    "InMemorySink",
+    "JsonlFileSink",
+    "TamperDetectedError",
+]
+
+__version__ = "0.1.0"
